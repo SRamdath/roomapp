6c436dcf3564b5fe2d1f0fd68b3b3e7d3009c9bf
SRamdath <49769553+SRamdath@users.noreply.github.com>
2025-07-28 05:24:17 -0400

    Update maintenenceapp.py

--- maintenenceapp.py
+++ maintenenceapp.py
@@ -138,6 +138,3 @@ if st.button("Parse"):
             parsed = parse_form(sentence)
             st.markdown(f"**Example {i}:** `{sentence}`")
             st.json(parsed)
-            parsed = parse_form(sentence)
-            st.markdown(f"**Example {i}:** `{sentence}`")
-            st.json(parsed)
